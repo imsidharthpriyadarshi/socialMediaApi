517445967ce8d804a5ebb9a48273dc6c8f505ea2
Sidharth priyadarshi <imsidharthpriyadarshi@gmail.com>
2022-05-22 14:06:16 +0530

    remove default post image

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class Post(Base):
     title = Column(String, nullable = False)
     content = Column(String, nullable=False)
     published = Column(Boolean, server_default='TRUE')
-    image_path= Column(String,server_default= 'photos/default.jpg')
+    image_path= Column(String)
     
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default =text('now()'))
     owner_id = Column(Integer,ForeignKey("users.id",ondelete="CASCADE"),nullable = False)
